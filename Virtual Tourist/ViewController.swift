f9a56b78bc420d97f3248e31bb9d833beeb06213
Lyn Al Masri <lynmasry@gmail.com>
2018-12-16 11:21:04 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Lyn Almasri on 12/16/18.
+//  Copyright © 2018 lynmasri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
